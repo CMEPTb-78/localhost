7998c55a6cd1cbb7fd6dbf544917920901b392f2
Denis <dengamor61@gmail.com>
2026-03-02 11:25:28 +0300

    continue working

--- test/lab2/task5/script.php
+++ test/lab2/task5/script.php
@@ -1,11 +1,16 @@
 <?php
     $counter = 0;
+    $nado = 0;
     for ($i = 100000; $i < 1000000; $i++) {
         $istr = (string) $i;
         if ((int) $istr[0] + (int) $istr[1] + (int) $istr[2] == (int) $istr[3] + (int) $istr[4] + (int) $istr[5]) {
             $counter++;
+            if ($nado <= 200 ) {
+                echo $i , "\n";
+                $nado++;
+            } 
         }
     }
-    $res = $counter * 10000; 
+    $res = $counter * 1; 
     echo $res,'<br>';
-    echo ($res / 9000000000) * 100;
+    echo ($res / 900000) * 100;
